dc2b1c3f8cfce580ad667cb8c186e7ead5259526
Olivier Lévy <olevy80@gmail.com>
2020-11-24 18:35:36 +0100

    externalize env keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,17 @@
 import { firebaseConfig } from './firebase';
 export const environment = {
   production: true,
-  firebaseConfig,
+  firebaseConfig: {
+    apiKey: '${process.env.FIREBASE_API_KEY}' || firebaseConfig.apiKey,
+    authDomain: 'olivier-levy.firebaseapp.com',
+    databaseURL: 'https://olivier-levy.firebaseio.com',
+    projectId: 'olivier-levy',
+    storageBucket: 'olivier-levy.appspot.com',
+    messagingSenderId:
+      '${process.env.FIREBASE_MESSAGING_SENDER_ID}' ||
+      firebaseConfig.messagingSenderId,
+    appId: '${process.env.FIREBASE_APP_ID}' || firebaseConfig.appId,
+    measurementId:
+      '${process.env.FIREBASE_MEASUREMENT_ID}' || firebaseConfig.measurementId,
+  },
 };
