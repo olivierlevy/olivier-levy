83210df5af874102f247d6b12b8e0e1a0b622f83
Olivier Lévy <olevy80@gmail.com>
2020-11-24 21:29:34 +0100

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,5 @@
 import { firebaseConfig } from './firebase';
 export const environment = {
   production: true,
-  firebaseConfig: {
-    apiKey: '${process.env.FIREBASE_API_KEY}' || firebaseConfig.apiKey,
-    authDomain: 'olivier-levy.firebaseapp.com',
-    databaseURL: 'https://olivier-levy.firebaseio.com',
-    projectId: 'olivier-levy',
-    storageBucket: 'olivier-levy.appspot.com',
-    messagingSenderId:
-      '${process.env.FIREBASE_MESSAGING_SENDER_ID}' ||
-      firebaseConfig.messagingSenderId,
-    appId: '${process.env.FIREBASE_APP_ID}' || firebaseConfig.appId,
-    measurementId:
-      '${process.env.FIREBASE_MEASUREMENT_ID}' || firebaseConfig.measurementId,
-  },
+  firebaseConfig,
 };
---
+++ src/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCVZFGIaVohMv_oRby-3pA9rOYF1NZhtQY',
+  authDomain: 'olivier-levy.firebaseapp.com',
+  databaseURL: 'https://olivier-levy.firebaseio.com',
+  projectId: 'olivier-levy',
+  storageBucket: 'olivier-levy.appspot.com',
+  messagingSenderId: '604812869697',
+  appId: '1:604812869697:web:d6ad854f321c10ad61c6c0',
+  measurementId: 'G-TXYKCT5P2N',
+};
